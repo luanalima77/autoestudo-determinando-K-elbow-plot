4ecb3dcf0fd2501a5d70a0658f659427cffee6a3
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-09-04 07:05:16 -0300

    feat: importar KMeans da biblioteca scikit-learn

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn
 
+#Importar KMeans da biblioteca scikit-learn.
+from sklearn.cluster import KMeans
+
 #Importar dataset
 dataset = pd.read_csv("Mall_Customers.csv")
 X = dataset.iloc[:, [3,4]].values
