0c11dac1ac647317b47d2cf085287ef66b9d8888
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-09-04 07:04:03 -0300

    feat: importar dataset

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sklearn
+
+#Importar dataset
+dataset = pd.read_csv("Mall_Customers.csv")
+X = dataset.iloc[:, [3,4]].values
