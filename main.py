88eeeb748ff9ca2f261540332f7439f004c4e0d9
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-09-04 07:13:30 -0300

    feat: calcular WCSS

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ from sklearn.cluster import KMeans
 #Importar dataset
 dataset = pd.read_csv("Mall_Customers.csv")
 X = dataset.iloc[:, [3,4]].values
+
+#Calcular WCSS (o init inicializa o centroide).
+wcss = [] 
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters=i, init="k-means++", random_state=42)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+    
