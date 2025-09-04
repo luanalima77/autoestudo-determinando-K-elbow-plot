03fabe277a476950ab877378eea1da5dd4814612
Luana Lima <luana.lima@sou.inteli.edu.br>
2025-09-04 07:21:06 -0300

    feat: plotar elbow plot

--- main.py
+++ main.py
@@ -17,4 +17,13 @@ for i in range(1, 11):
     kmeans = KMeans(n_clusters=i, init="k-means++", random_state=42)
     kmeans.fit(X)
     wcss.append(kmeans.inertia_)
-    
+
+#PLOTAR O ELBOW PLOT.
+plt.figure(figsize=(8,5))
+#Definindo o eixo x de 1 a 10 e o eixo y com o wcss.
+plt.plot(range(1,11), wcss, marker="o", linestyle = "--")
+
+#Colocando informações textuais do gráfico.
+plt.title("Elbow plot")
+plt.show()
+
